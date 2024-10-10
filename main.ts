e4e713f8d1bd98cb3727882052ef9c3f5579c235
Setia Trianto <setia.trianto@gmail.com>
2024-10-10 14:03:53 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.showLongText("Time is waktu, karena waktu adalah pedang", DialogLayout.Bottom)
+    music.play(music.melodyPlayable(music.wawawawaa), music.PlaybackMode.InBackground)
+    effects.confetti.startScreenEffect(500)
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
